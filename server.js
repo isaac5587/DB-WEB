1e7d29a02b48ec64ef3704f98e6f76e5105808a5
Isaac Ansumana <isaac.ansumana@gmail.com>
2020-11-10 06:51:48 -0800

    Made the necesary changes that we did in class.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 const request = require('request');
-const sql = require('mssql');
+const sql = require('mssql');            //interact with database
 const bodyParser = require('body-parser');
 //const routes = require( "./routes" );
 
@@ -18,8 +18,32 @@ var server = app.listen(process.env.PORT || 5000, listen);
 module.exports = server;
 path = require('path');
 
+let dbConnection = "mssql://isaac.ansumana:power@ev-compsci-01.principia.local/isaac.ansumana";
+app.get('/', async (red,res) => {
+      try{
+        console.log(dbConnection);
 
+        // Connecting to DB
+        sql.connect(dbConnection, function(err){
+          if(err) console.log(err);
 
+          // Create a new request
+          let sqlRequest = new sql.Request();
+          let sqlQuery = 'SELECT ID, firstName, lastName, favorites FROM Customer;';
+          sqlRequest.query(sqlQuery, function(err, data){
+            if (err) console.log(err);
+            console.log(data);
+            console.table(data.recordset);
+            sql.close();
+            res.sendFile(path.join(__dirname + '/public/index.html'));
+          });
+        });
+      }
+      catch (err){
+        res.status(500);
+        res.send(err.message);
+      }
+});
 
 
 // ***********************************************
@@ -31,7 +55,7 @@ app.set('views', __dirname);
 app.use(express.urlencoded({ extended: true }))
 
 // This call back just tells us that the server has started
-function listen() {
+function listen() {  // listens for local requests
   var host = server.address().address;
   var port = server.address().port;
   console.log('Listening at http://' + host + ':' + port);
